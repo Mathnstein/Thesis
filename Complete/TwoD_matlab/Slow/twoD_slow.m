d5e4f54407bd591a26710b44159464ae29590023
Cody Griffith <cody.griffith94@gmail.com>
2018-01-17 12:22:04 -0800

    Added twoD_slow to the pipeline

--- Complete/TwoD_matlab/Slow/twoD_slow.m
+++ Complete/TwoD_matlab/Slow/twoD_slow.m
@@ -5,192 +5,154 @@
 %solution for different choices of epsilon.
 
 % Load function folder
-addpath('C:\Users\codyg\Desktop\MSc_Thesis\Cody\Complete\Functions')
+addpath('C:\Users\codyg\Desktop\MSc_Thesis\Cody\trunk\Complete\Functions')
+cd('C:\Users\codyg\Desktop\MSc_Thesis\Cody\trunk\Complete\Graphs\TwoD_Slow')
 
+criteria = .5;
+% Previous criteria was -C(1,1), the eigvector component
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Code Begin%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Start on NS stable branch
-%Works for n3<1
-%In progress for making work for n3>1
+% Start on NS stable branch
+% Works for n3<1
 
-%Parameters
-n=100;
-eta1=4;eta3=.375;
-h=.01;
-
-%Fixed parameter system
-start=eta1*eta3-1;stop=eta1*eta3+1;
-eta2=linspace(start,stop,n);
-vSteady=zeros(1,length(eta2));
-tSteady=vSteady;
-
-%Create equilibrium vector
-for i=1:length(eta2)
-    %Non-dim Stommel Equations
-    vDE=@(t,V,T)((eta1-eta2(i))-V*abs(V)-T+eta3*(T-V));
-    tDE=@(t,V,T)(eta1-T*(1+abs(V)));
-    if eta3<= 1
-        vInit=-eta2(i)/eta1+eta3;
-        tInit=eta1/(1-vInit);
-    else
-        vInit=(-eta2(i))/eta1+eta1;
-        tInit=eta1/(1+vInit);
-    end
-    
-    
-    %Solve equations for long term equilibrium, step size h
-    [~,V,T]=RK2sys(vDE,tDE,vInit,tInit,0,100,h);
-    
-    %Keep steady state
-    vSteady(i)=V(end);
-    tSteady(i)=T(end);
-end
-Min=min(vSteady);
-Max=max(vSteady);
+% Parameters
+n = 400;
+eta1 = 4; eta3 = .375;
+h = .01;
 
 %-------------------------------------------------
-%Ramped system 
-eps=.01;
-time=(stop-start)/(abs(eps));
+% Slow system 
+eps = .01;
+start = eta1*eta3-1;stop=eta1*eta3+1;
+time = (stop-start)/(abs(eps));
 
-%Non-dim Stommel Equations
-vDE=@(t,V,T,eta2)((eta1-eta2)-V*abs(V)-T+eta3*(T-V));
-tDE=@(t,V,T,eta2)(eta1-T*(1+abs(V)));
+% Non-dim Stommel Equations
+vDE = @(t,V,T,eta2)((eta1-eta2)-V*abs(V)-T+eta3*(T-V));
+tDE = @(t,V,T,eta2)(eta1-T*(1+abs(V)));
+eta2DE = @(t,V,T,eta2)(-eps);
 
-if eta3<=1
-    eta2DE=@(t,V,T,eta2)(-eps);
-else
-    eta2DE=@(t,V,T,eta2)(eps);
-end
+eta2Init = stop;
+vInit = -stop/eta1+eta3;
+tInit = eta1/(1-vInit);
 
-if eta3<=1
-    eta2Init=stop;
-    vInit=-stop/eta1+eta3;
-    tInit=eta1/(1-vInit);
-else
-    eta2Init=start;
-    vInit=start/eta1+eta3;
-    tInit=eta1/(1+vInit);
-end
+% Solve equations for numerical soln, step size h
+[~,Vnum,Tnum,eta2num] = RK2sys3(vDE,tDE,eta2DE,vInit,tInit,eta2Init,0,time,h);
 
-%Solve equations for numerical soln, step size h
-[~,Vnum,Tnum,eta2num]=RK2sys3(vDE,tDE,eta2DE,vInit,tInit,eta2Init,0,time,h);
+% Ignore transient behavior
+Vnum = Vnum(100:end);
+Tnum = Tnum(100:end);
+eta2num = eta2num(100:end);
 
+% Determine the tipping points for each case
+A=[eta3 1-eta3;eta1 1];
+[C,eigs] = eig(A);
+tipvec1 = eta1*eta3-eps*log(eps)/eigs(1,1);
+tippredvec = tipvec1*ones(1,n);
+yvec = linspace(Min,Max,n);
 
-%Determine the tipping points for each case
-if eta3<=1
-    A=[eta3 1-eta3;eta1 1];
-    [C,eigs]=eig(A);
-    tip1=eta1*eta3-eps*log(eps)/eigs(1,1);
-    tipx1=tip1*ones(1,n);
-    tipy=linspace(Min,Max,n);
-
-    realtip=eta2num(find(Vnum>-C(1,1),1));
-    realtipvec=realtip*ones(1,n);
-else 
-    A=[-eta3 1-eta3;-eta1 -1];
-    [C,eigs]=eig(A);
-    tip1=eta1*eta3+eps*log(abs(eps))/eigs(1,1);
-    tipx1=tip1*ones(1,n);
-    tipy=linspace(Min,Max,n);
-
-    realtip=eta2num(find(Vnum<C(1,1),1));
-    realtipvec=realtip*ones(1,n);
-end
+tipactual=eta2num(find(Vnum>criteria,1));
+tipactualvec=tipactual*ones(1,n);
 
 %--------------------Plot the dynamics plot--------------------------
 
 figure(1)
-%plot(eta2,vSteady,'r')
-z=@(eta2,V)(eta1-eta2)-V*abs(V)-eta1/(1+abs(V))+eta3*(eta1/(1+abs(V))-V);
-h1=ezplot(z,[eta1*eta3-1,eta1*eta3+1,-.5,1.5]);
+z = @(eta2,V)(eta1-eta2)-V*abs(V)-eta1/(1+abs(V))+eta3*(eta1/(1+abs(V))-V);
+h1 = ezplot(z,[eta1*eta3-1,eta1*eta3+1,-.5,1.5]);
 set(h1,'linestyle','-.','color','k')
 hold on
-z=@(eta2,V)(eta1-eta2)-V*abs(V)-eta1/(1+abs(V))+eta3*(eta1/(1+abs(V))-V);
-h2=ezplot(z,[eta1*eta3-1,eta1*eta3+1,-.5,0]);
+z = @(eta2,V)(eta1-eta2)-V*abs(V)-eta1/(1+abs(V))+eta3*(eta1/(1+abs(V))-V);
+h2 = ezplot(z,[eta1*eta3-1,eta1*eta3+1,-.5,0]);
 set(h2,'color','r','linewidth',2)
 hold on
-z=@(eta2,V)(eta1-eta2)-V*abs(V)-eta1/(1+abs(V))+eta3*(eta1/(1+abs(V))-V);
-h3=ezplot(z,[eta1*eta3-1,eta1*eta3+1,.426651,1.5]);
+z = @(eta2,V)(eta1-eta2)-V*abs(V)-eta1/(1+abs(V))+eta3*(eta1/(1+abs(V))-V);
+h3 = ezplot(z,[eta1*eta3-1,eta1*eta3+1,.426651,1.5]);
 set(h3,'color','r','linewidth',2)
 
 xlabel('\eta_2')
 ylabel('V')
 title('')
 hold on
-plot(eta2num,Vnum,'k--')
-%plot(realtipvec,tipy,'b')
-%plot(tipx1,tipy,'b-.')
+plot(eta2num,Vnum,'k--','linewidth',2)
+plot(tippredvec,yvec,'b')
+plot(tipactualvec,yvec,'k--')
 xlim([start,stop])
-%title(['V Equilibria for \epsilon=' num2str(eps)])
 xlabel('\eta_2')
 ylabel('V')
-%legend('Numerical w/ Fixed \eta_2','Ramped System','Tipping Criteria','Tipping Point: \lambda_1')
 
-fprintf('Tipping point for %f is %f\nPredicted tip is %f\nError: %f\n',eps,realtip,tip1,abs(realtip-tip1))
+%Zoom
+
+plot(tippredvec,yvec,'b')
+plot(tipactualvec,yvec,'k--')
+xlim([1.44 1.6])
+ylim([-.05 .2])
+
+print('-f1','slow_bif_zoom','-djpeg');
+
+
+% T plot
+V=linspace(-2,2,n);
+T=zeros(length(V),1);
+func=@(V)(eta1/(1+abs(V)));
+for i=1:length(V)
+    T(i)=func(V(i));
+end
+
+figure(2)
+plot(V,T,'r','linewidth',2)
+hold on
+plot(Vnum,Tnum,'k','linewidth',2)
+xlabel('V')
+ylabel('T')
+xlim([-.75 Max])
+ylim([1.5 eta1])
+
+
+print('-f2','slow_bif_Tplot','-djpeg');
+
+fprintf('Tipping point for %f is %f\nPredicted tip is %f\nError: %f\n',eps,tipactual,tipvec1,abs(tipactual-tipvec1))
+
 
 %%%%%%%%%%%%%%Comparison plot for different epsilon%%%%%%%%%%%%%%%%%%%%%%%
 %3-system for tipping
-epsvec=linspace(.0001,.2,20);
-tipping=zeros(1,length(epsvec));
-tip1=zeros(1,length(epsvec));
-tip2=zeros(1,length(epsvec));
-for j=1:length(epsvec)
-eps=epsvec(j);
-time=(stop-start)/(abs(eps));
+epsvec = linspace(.001,.2,20);
+M = length(epsvec);
+tipactualvec = zeros(1,M);
+tipvec1 = zeros(1,M);
+tipvec2 = zeros(1,M);
 
-%Non-dim Stommel Equations
-vDE=@(t,V,T,eta2)((eta1-eta2)-V*abs(V)-T+eta3*(T-V));
-tDE=@(t,V,T,eta2)(eta1-T*(1+abs(V)));
- if eta3<=1
-     eta2DE=@(t,V,T,eta2)(-eps);
- else
-       eta2DE=@(t,V,T,eta2)(eps);
- end
+for j = 1:M
+    eps = epsvec(j);
+    time = (stop-start)/(abs(eps));
 
-    if eta3<=1
-        eta2Init=stop;
-        vInit=-stop/eta1+eta3;
-        tInit=eta1/(1-vInit);
-    else
-        eta2Init=start;
-        vInit=start/eta1+eta3;
-        tInit=eta1/(1-vInit);
-    end
+    % Non-dim Stommel Equations
+    vDE = @(t,V,T,eta2)((eta1-eta2)-V*abs(V)-T+eta3*(T-V));
+    tDE = @(t,V,T,eta2)(eta1-T*(1+abs(V)));
+    eta2DE = @(t,V,T,eta2)(-eps);
+
+    eta2Init = stop;
+    vInit = -stop/eta1+eta3;
+    tInit = eta1/(1-vInit);
     
-    %Solve equations for numerical soln, step size .01
-    [~,Vnum,Tnum,eta2num]=RK2sys3(vDE,tDE,eta2DE,vInit,tInit,eta2Init,0,time,h);
+    % Solve equations for numerical soln, step size .01
+    [~,Vnum,Tnum,eta2num] = RK2sys3(vDE,tDE,eta2DE,vInit,tInit,eta2Init,0,time,h);
     
-    if eta3<=1
-        realtip=eta2num(find(Vnum>-C(1,1),1));
-        tipping(j)=realtip-eta1*eta3;
-        %Asymptotic Function
-        asympfunc1=@(epsilon)(-epsilon*log(epsilon)/eigs(1,1));
-        asympfunc2=@(epsilon)(-epsilon*log(epsilon)/eigs(2,2));
-        %Form Asymptotic line
-        tip1(j)=asympfunc1(epsvec(j));
-        tip2(j)=asympfunc2(epsvec(j));
-    else
-        realtip=eta2num(find(Vnum<C(1,1),1));
-        tipping(j)=realtip-eta1*eta3;
-        %Asymptotic Function
-        asympfunc1=@(epsilon)(epsilon*log(epsilon)/eigs(1,1));
-        asympfunc2=@(epsilon)(epsilon*log(epsilon)/eigs(2,2));
-        %Form Asymptotic line
-        tip1(j)=asympfunc1(epsvec(j));
-        tip2(j)=asympfunc2(epsvec(j));
-    end
-
+     tipactual = eta2num(find(Vnum>criteria,1));
+     tipactualvec(j) = tipactual-eta1*eta3;
+     % Asymptotic Function
+     asympfunc1 = @(epsilon)(-epsilon*log(epsilon)/eigs(1,1));
+     asympfunc2 = @(epsilon)(-epsilon*log(epsilon)/eigs(2,2));
+     % Form Asymptotic line
+     tipvec1(j) = asympfunc1(epsvec(j));
+     tipvec2(j) = asympfunc2(epsvec(j));
 end
 
 %Plot the Asymptotic solution vs TIpping point
-figure(2)
-plot(epsvec,tip1,'k','linewidth',2)
+figure(3)
+plot(epsvec,tipvec1,'k','linewidth',2)
 hold on
-%plot(epsvec,tip2,'b')
-plot(epsvec,tipping,'r*')
+plot(epsvec,tipactualvec,'r*')
 hold off
-%legend('Asymptotic:\lambda_1','Asymptotic: \lambda_2','Tipping Point')
-%title('Asymptotic vs Full soln')
 xlabel('\epsilon')
 ylabel('\eta_2-\eta_{2ns}')
+
+print('-f3','slow_epscomp','-djpeg')
